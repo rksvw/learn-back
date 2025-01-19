248a66b08cbefb0d663796da3f26e1fcaeb61e6b
rksvw <ritik.vw98@gmail.com>
2025-01-19 17:49:42 +0530

    did the low level crawling from zsh command line using process.args to check its work

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,14 @@
-console.log("hello, world");
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("too many command line args");
+    process.exit(1);
+  }
+  const baseURL = process.argv[2];
+  console.log(`Starting crawl of ${baseURL}`);
+}
+
+main();
