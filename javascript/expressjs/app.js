70aeb85e510c5fb10d26b79c6c822f33b02eedec
rksvw <ritik.vw98@gmail.com>
2025-02-16 17:58:36 +0530

    baby static server

---
+++ javascript/expressjs/app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    res.end("Welcome to our home page");
+  }
+  if (req.url === "/about") {
+    res.end(`Here is my about page`);
+  }
+  res.end(`
+    <h1>Hello World</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/home"><button type="submit">Back to Home</button></a>`);
+});
+
+server.listen(5030, (err) => {
+  console.log(`Server is running on port 5030`);
+});
